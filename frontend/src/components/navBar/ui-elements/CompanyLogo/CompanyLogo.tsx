f7539c996bee2779fca1cae6fcff38a41d2417f0
Harry Rhodes <hjtrhodes@gmail.com>
2024-02-01 21:11:52 +0000

    removed require statement from tailwind config

--- frontend/src/components/navBar/ui-elements/CompanyLogo/CompanyLogo.tsx
+++ frontend/src/components/navBar/ui-elements/CompanyLogo/CompanyLogo.tsx
@@ -3,7 +3,7 @@ import Logo from "../../../../assets/AcebookLogo.png"
 
 export const CompanyLogo = () => {
   return (
-    <Link to="/" className="flex items-center">
+    <Link to="/posts" className="flex items-center">
       <span className="self-center text-xl font-bold text-gray-800 whitespace-nowrap dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400">
       <img src={Logo} alt="Company Logo" className="h-8 w-8 mr-2 border-none mb-2" />
       </span>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
 }
 
